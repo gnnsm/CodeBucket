c3c7857c6f75e631f444a8dcc4e6011c6ca3efe5
gnnsm <53880669+gnnsm@users.noreply.github.com>
2024-01-27 14:54:44 +0300

    Create dice.py

---
+++ dice.py
@@ -0,0 +1,35 @@
+import re
+import random
+
+
+def calculate(number, dice, append):
+    result = 0
+    while number > 0:
+        throwing = random.randint(1, dice)
+        result += throwing
+        # print(f'Dice #{number}: {throwing}')
+        number -= 1
+    result += append
+    return result
+
+
+def dice(command: str):
+    append = 0
+    command = re.sub('\s+', '', command).lower()
+    if not bool(re.match(r'^(\d+)?d\d+(\+\d+)?$$', command)):
+        return ':angry:'
+    else:
+        split_append = re.split(r'\+', command)
+        if len(split_append) != 1:
+            append = int(split_append[-1])
+        split_d = re.split(r'd', split_append[0])
+        if len(split_d[0]) == 0:
+            number = 1
+        else:
+            number = int(split_d[0])
+        die = int(split_d[1])
+        return calculate(number, die, append)
+
+
+if __name__ == "__main__":
+    print(dice('2d6+5'))
